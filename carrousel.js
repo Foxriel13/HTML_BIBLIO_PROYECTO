f9e30550558b280b4a3f4fbe14f8b6984e78e75d
Luis Miguel Bravo <luismiguelbravo13@gmail.com>
2024-06-05 21:51:44 +0200

    si

--- carrousel.js
+++ carrousel.js
@@ -1,77 +1,4 @@
-        function asignarAnimacion(tarjeta, claseActual, claseSiguiente){
-            tarjeta.style.animation = claseActual.toUpperCase() + 'to' + claseSiguiente.toUpperCase() + ' 1s linear forwards';
-            tarjeta.className = 'tarjeta ' + claseSiguiente.toLowerCase();
-        }
-
-        var $prev = document.getElementById('prev');
-        var $next = document.getElementById('next');
-
-        $prev.addEventListener('click', function(){
-            const $tarjetas = Array.from( document.getElementsByClassName('tarjeta') )
-            $tarjetas.forEach(function($tarjeta){
-                switch ($tarjeta.className.toString().substr(8, 1)){
-                    case 'a':
-                        asignarAnimacion($tarjeta, 'a', 'e');
-                        break;
-                    case 'b':
-                        asignarAnimacion($tarjeta, 'b', 'a');
-                        break;
-                    case 'c':
-                        asignarAnimacion($tarjeta, 'c', 'b');
-                        break;
-                    case 'd':
-                        asignarAnimacion($tarjeta, 'd', 'c');
-                        break;
-                    case 'e':
-                        asignarAnimacion($tarjeta, 'e', 'd');
-                        break;
-                }
-            })
-        })
-
-        $next.addEventListener('click', function(){
-            const $tarjetas = Array.from( document.getElementsByClassName('tarjeta') )
-            $tarjetas.forEach(function($tarjeta){
-                switch ($tarjeta.className.toString().substr(8, 1)){
-                    case 'a':
-                        asignarAnimacion($tarjeta, 'a', 'b');
-                        break;
-                    case 'b':
-                        asignarAnimacion($tarjeta, 'b', 'c');
-                        break;
-                    case 'c':
-                        asignarAnimacion($tarjeta, 'c', 'd');
-                        break;
-                    case 'd':
-                        asignarAnimacion($tarjeta, 'd', 'e');
-                        break;
-                    case 'e':
-                        asignarAnimacion($tarjeta, 'e', 'a');
-                        break;
-                }
-            })
-        })
-
-        function cambiarColor() {
-            var colores = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff']; 
-            var palabra = document.getElementById('palabra');
-            var letras = palabra.textContent.split('');
-          
-    
-            palabra.innerHTML = '';
-          
-
-            letras.forEach(function(letra) {
-              var span = document.createElement('span'); 
-              span.textContent = letra; 
-              var colorAleatorio = colores[Math.floor(Math.random() * colores.length)]; 
-              span.style.color = colorAleatorio;
-              palabra.appendChild(span); 
-            });
-          }
-          setInterval(cambiarColor, 500);
-
-          let contador = 0;
+let contador = 0
 const maxContador = 120;
 const intervalo = 10;
 
@@ -82,3 +9,50 @@ const intervalId = setInterval(() => {
     }
     contador++;
 }, intervalo);
+
+
+let imagenes = ["./Imagenes/milibro.jpg", "./Imagenes/mine.jpg", "./Imagenes/Don_Quijote_de_la_Mancha.jpg", "./Imagenes/saw.jpg"];
+let descripcion = ["En un lejano parque de atracciones y en plena misión secreta para defender a su amada luna de un peligroso emperador, la joven Claudia, hija de Metallus, conoce a Knaach, y juntos se embarcan en una odisea de sucesos desafortunados que desatarán una verdadera guerra galáctica.","Hay muchas formas de jugar a Minecraft, y una de las más populares es el modo Supervivencia, en el que tendrás que forjar tu propio camino en el juego, solo con los bloques que encuentres para fabricar cosas, y te enfrentarás a muchos peligros con la certeza de que, con un solo movimiento en falso, podrías perderlo todo. Es muy emocionante, ¡y muy difícil!","Es el personaje principal de la obra, y tiene como características físicas: De aspecto pálido, alto, delgado, nariz puntiaguda, tiene Barba y bigote. Es el protagonista de la novela y constituye un consagrado mito de la literatura universal, y el más universal y profundo de la literatura española.","La película cuenta una narración no lineal que gira en torno al misterio del asesino Jigsaw, que pone a prueba la voluntad de vivir de sus víctimas sometiéndolas a juegos mortales en los que deben infligirse un gran dolor físico para sobrevivir."];
+
+var posicion = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    const btnizquierda = document.getElementById('margintopmovimientoizquierda');
+    const btnderecha = document.getElementById('margintopmovimientoderecha');
+    btnizquierda.addEventListener('click', function() {
+        var card1 = document.getElementById('card1')
+var card2 = document.getElementById('card2')
+var card3 = document.getElementById('card3')
+var desc1 = document.getElementById('descripcion1')
+var desc2 = document.getElementById('descripcion2')
+var desc3 = document.getElementById('descripcion3')
+        if(posicion + 2 >= 4){
+            posicion = 0;
+        }
+        card1.src = imagenes[posicion];
+        card2.src = imagenes[posicion+1];
+        card3.src = imagenes[posicion+2];
+        desc1.textContent = descripcion[posicion]
+        desc2.textContent = descripcion[posicion+1]
+        desc3.textContent = descripcion[posicion+2]
+        posicion +=1
+    });
+
+    btnderecha.addEventListener('click', function() {
+        var card1 = document.getElementById('card1')
+var card2 = document.getElementById('card2')
+var card3 = document.getElementById('card3')
+var desc1 = document.getElementById('descripcion1')
+var desc2 = document.getElementById('descripcion2')
+var desc3 = document.getElementById('descripcion3')
+        if(posicion - 2 <= -1){
+            posicion = 3;
+        }
+        card1.src = imagenes[posicion-2];
+        card2.src = imagenes[posicion-1];
+        card3.src = imagenes[posicion];
+        desc1.textContent = descripcion[posicion-2]
+        desc2.textContent = descripcion[posicion-1]
+        desc3.textContent = descripcion[posicion]
+        posicion -=1
+    });
+});
