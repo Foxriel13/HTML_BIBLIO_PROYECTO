b7c2a912a8dfb5ab021353151508bec6ffdee336
Luis <luismiguelbravo13@gmail.com>
2024-06-09 18:40:16 +0200

    arreglos index

--- carrousel.js
+++ carrousel.js
@@ -15,16 +15,18 @@ let imagenes = ["./Imagenes/milibro.jpg", "./Imagenes/mine.jpg", "./Imagenes/Don
 let descripcion = ["En un lejano parque de atracciones y en plena misión secreta para defender a su amada luna de un peligroso emperador, la joven Claudia, hija de Metallus, conoce a Knaach, y juntos se embarcan en una odisea de sucesos desafortunados que desatarán una verdadera guerra galáctica.","Hay muchas formas de jugar a Minecraft, y una de las más populares es el modo Supervivencia, en el que tendrás que forjar tu propio camino en el juego, solo con los bloques que encuentres para fabricar cosas, y te enfrentarás a muchos peligros con la certeza de que, con un solo movimiento en falso, podrías perderlo todo. Es muy emocionante, ¡y muy difícil!","Es el personaje principal de la obra, y tiene como características físicas: De aspecto pálido, alto, delgado, nariz puntiaguda, tiene Barba y bigote. Es el protagonista de la novela y constituye un consagrado mito de la literatura universal, y el más universal y profundo de la literatura española.","La película cuenta una narración no lineal que gira en torno al misterio del asesino Jigsaw, que pone a prueba la voluntad de vivir de sus víctimas sometiéndolas a juegos mortales en los que deben infligirse un gran dolor físico para sobrevivir."];
 
 var posicion = 0;
-document.addEventListener('DOMContentLoaded', function() {
     const btnizquierda = document.getElementById('margintopmovimientoizquierda');
     const btnderecha = document.getElementById('margintopmovimientoderecha');
     btnizquierda.addEventListener('click', function() {
-        var card1 = document.getElementById('card1')
+var card1 = document.getElementById('card1')
 var card2 = document.getElementById('card2')
 var card3 = document.getElementById('card3')
 var desc1 = document.getElementById('descripcion1')
 var desc2 = document.getElementById('descripcion2')
 var desc3 = document.getElementById('descripcion3')
+var enlace1 = document.getElementById('enlace1')
+var enlace2 = document.getElementById('enlace2')
+var enlace3 = document.getElementById('enlace3')
         if(posicion + 2 >= 4){
             posicion = 0;
         }
@@ -35,6 +37,21 @@ var desc3 = document.getElementById('descripcion3')
         desc2.textContent = descripcion[posicion+1]
         desc3.textContent = descripcion[posicion+2]
         posicion +=1
+        if (posicion == 3){
+            enlace1.href = "./detalle.html"
+        }else{
+            enlace1.href = "#"
+        }
+        if (posicion+1 == 3){
+            enlace2.href = "./detalle.html"
+        }else{
+            enlace2.href = "#"
+        }
+        if (posicion+2 == 3){
+            enlace3.href = "./detalle.html"
+        }else{
+            enlace3.href = "#"
+        }
     });
 
     btnderecha.addEventListener('click', function() {
@@ -45,7 +62,7 @@ var desc1 = document.getElementById('descripcion1')
 var desc2 = document.getElementById('descripcion2')
 var desc3 = document.getElementById('descripcion3')
         if(posicion - 2 <= -1){
-            posicion = 3;
+            posicion = imagenes.length-1;
         }
         card1.src = imagenes[posicion-2];
         card2.src = imagenes[posicion-1];
@@ -54,5 +71,20 @@ var desc3 = document.getElementById('descripcion3')
         desc2.textContent = descripcion[posicion-1]
         desc3.textContent = descripcion[posicion]
         posicion -=1
+
+        if (posicion == 3){
+            enlace1.href = "./detalle.html"
+        }else{
+            enlace1.href = "#"
+        }
+        if (posicion+1 == 3){
+            enlace2.href = "./detalle.html"
+        }else{
+            enlace2.href = "#"
+        }
+        if (posicion+2 == 3){
+            enlace3.href = "./detalle.html"
+        }else{
+            enlace3.href = "#"
+        }
     });
-});
